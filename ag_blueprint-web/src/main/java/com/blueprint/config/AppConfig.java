0b5f692f71c37495c3b7d92063613fa29d7dcfc1
Mahidhar <mahi.mullapudi@gmail.com>
2016-11-02 19:25:43 -0400

    Initial Commit for the project which acts as a base for using spring mvc, log4j2+slf4j, jnuit4

---
+++ ag_blueprint-web/src/main/java/com/blueprint/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.blueprint.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.blueprint.web.controllers"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+
+}
---
+++ ag_blueprint-web/src/main/java/com/blueprint/web/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package com.blueprint.web.controllers;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.LoggerFactory;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String indexMethod() {
+        Logger logger = LoggerFactory.getLogger(IndexController.class);
+        logger.debug("Inside indexMethod of IndexController");        
+        return "index";
+    }
+
+}
